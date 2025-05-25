b279d386a012f57ef306a59a6095be2451d44752
Leo Esguerra <leo.esguerra@gmail.com>
2025-05-25 00:10:35 -0600

    Create CharacterAbilities component, moved from CharacterClass

---
+++ Source/ItTakesToque/Components/CharacterAbilities.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/Character.h"
+#include "CharacterAbilities.generated.h"
+
+class UInputAction;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ITTAKESTOQUE_API UCharacterAbilities : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UCharacterAbilities();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
+	TMap<TObjectPtr<UInputAction>, TSubclassOf<class UGameplayAbility>> Abilities;
+};
---
+++ Source/ItTakesToque/Components/My2AbilitySystemComponent.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayAbilitySpec.h" // for FGameplayAbilitySpecHandle
+#include "EnhancedInputComponent.h" // for FInputBindingHandle
+#include "My2AbilitySystemComponent.generated.h"
+
+USTRUCT()
+struct FAbilityInputBinding
+{
+	GENERATED_BODY()
+
+	int32  InputID = 0;
+	uint32 OnPressedHandle = 0;
+	uint32 OnReleasedHandle = 0;
+	TArray<FGameplayAbilitySpecHandle> BoundAbilitiesStack;
+};
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType, Category="Custom", meta = (BlueprintSpawnableComponent))
+class ITTAKESTOQUE_API UMy2AbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+	public:
+	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
+	void SetInputBinding(UInputAction* InputAction, FGameplayAbilitySpecHandle AbilityHandle);
+
+	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
+	void ClearInputBinding(FGameplayAbilitySpecHandle AbilityHandle);
+
+	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
+	void ClearAbilityBindings(UInputAction* InputAction);
+
+	void SetInputComponent(UEnhancedInputComponent* InInputComponent);
+
+	private:
+	void OnAbilityInputPressed(UInputAction* InputAction);
+
+	void OnAbilityInputReleased(UInputAction* InputAction);
+
+	void RemoveEntry(UInputAction* InputAction);
+
+	void TryBindAbilityInput(UInputAction* InputAction, FAbilityInputBinding& AbilityInputBinding);
+
+	FGameplayAbilitySpec* FindAbilitySpec(FGameplayAbilitySpecHandle Handle);
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(transient)
+	TMap<UInputAction*, FAbilityInputBinding> MappedAbilities;
+
+	UPROPERTY(transient)
+	UEnhancedInputComponent* InputComponent;
+};
